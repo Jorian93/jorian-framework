8eb1badd4464937a7a9dca7c004b27462f8201e0
jorian93 <1977474361@qq.com>
2020-02-11 16:00:07 +0800

    修改配置文件和增加全局控制器

--- src/main/java/cn/jorian/jorianframework/core/GlobalController.java
+++ src/main/java/cn/jorian/jorianframework/core/GlobalController.java
@@ -2,6 +2,7 @@ package cn.jorian.jorianframework.core;
 
 import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
